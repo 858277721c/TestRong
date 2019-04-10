050c7d655b4639caafcc3a8bf1da7c880b31dcc0
zjun <565061763@qq.com>
2019-04-10 18:50:23 +0800

    demo

--- app/src/main/java/com/fanwe/trong/MainActivity.java
+++ app/src/main/java/com/fanwe/trong/MainActivity.java
@@ -9,6 +9,7 @@ import io.rong.imlib.RongIMClient;
 public class MainActivity extends AppCompatActivity
 {
     public static final String TAG = MainActivity.class.getSimpleName();
+    public static final String TOKEN = "LOHrQV6XMvO3G4Jxrraokq0NILXdP6D2szGAx3pIbvB4z80anU7DxmjPYouVZsmzI3K7988FFN7Bco9PPlV4YA==";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -16,8 +17,23 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final String token = "LOHrQV6XMvO3G4Jxrraokq0NILXdP6D2szGAx3pIbvB4z80anU7DxmjPYouVZsmzI3K7988FFN7Bco9PPlV4YA==";
-        RongIMClient.connect(token, new RongIMClient.ConnectCallback()
+        Log.i(TAG, this + "onCreate savedInstanceState:" + savedInstanceState);
+
+        if (savedInstanceState != null)
+        {
+            if (RongIMClient.getInstance().getCurrentConnectionStatus() != RongIMClient.ConnectionStatusListener.ConnectionStatus.CONNECTED)
+            {
+                connect();
+            }
+        } else
+        {
+            connect();
+        }
+    }
+
+    private void connect()
+    {
+        RongIMClient.connect(TOKEN, new RongIMClient.ConnectCallback()
         {
             @Override
             public void onTokenIncorrect()
